87992f89384b1e7d3d2772173fa1ec1418bd222a
Wilson Castillo <115800617+Wilson8jsn@users.noreply.github.com>
2023-12-21 11:24:47 -0500

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
-
-
 import React from 'react';
-import App from './components/Batman'; 
+import { Card } from 'antd';
 
-const MainApp: React.FC = () => {
-  return (
-    <div>
-      <h1>My Main App</h1>
-   
-      <App />
-    </div>
-  );
-};
+const { Meta } = Card;
 
-export default MainApp;
+const App: React.FC = () => (
+  <Card
+    hoverable
+    style={{ width: 240, marginTop: 200, justifyContent: 'center', marginLeft: '600px' }}
+    cover={<img alt="example" src="https://res.cloudinary.com/dcba9lt2s/image/upload/v1702476736/onbqxbd3sf9rmrrgvjkt.webp" />}
+  >
+    <Meta
+      title={
+        <div>
+          <span style={{ color: '#FF00E8' }}>REACT GRAHQL DEVELOP</span>
+          <br />
+          <span style={{ color: '#FF00E8' }}>FOR ANY DEVELOPMENT</span>
+        </div>
+      }
+      description="Best one bars to take friends and relatives, one bars to take friends and relatives"
+    />
+  </Card>
+);
+
+export default App;
